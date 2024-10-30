1617f241b32031c4b422e8c32b6bd9b6eb22298d
arseny <arsenyvolodko@gmail.com>
2024-10-30 23:15:44 +0300

    Добавление

--- SchoolPlanner/Pages/Teacher.xaml.cs
+++ SchoolPlanner/Pages/Teacher.xaml.cs
@@ -14,6 +14,11 @@ namespace SchoolPlanner.Pages
         private SolidColorBrush PrimaryBackgroundColor;
         private SolidColorBrush TextTertiaryColor;
         private Style placeholderTextBoxStyle;
+        private Style RoundedButtonStyle;
+        private PathGeometry OpenPathGeometry;
+        private PathGeometry BlockPathGeometry;
+        private PathGeometry SavePathGeometry;
+        private PathGeometry DeletePathGeometry;
 
         public Teacher(SchoolPlannerContext context)
         {
@@ -23,6 +28,11 @@ namespace SchoolPlanner.Pages
             PrimaryBackgroundColor = (SolidColorBrush)FindResource("PrimaryBackgroundColor");
             TextTertiaryColor = (SolidColorBrush)FindResource("TextTertiaryColor");
             placeholderTextBoxStyle = (Style)FindResource("PlaceholderTextBoxStyle");
+            RoundedButtonStyle = (Style)FindResource("RoundedButtonStyle");
+            OpenPathGeometry = (PathGeometry)FindResource("open");
+            BlockPathGeometry = (PathGeometry)FindResource("block");
+            SavePathGeometry = (PathGeometry)FindResource("save");
+            DeletePathGeometry = (PathGeometry)FindResource("delete");
 
             CreateStackPanelWithData();
         }
@@ -154,40 +164,93 @@ namespace SchoolPlanner.Pages
                     Margin = new Thickness(10)
                 };
 
+                System.Windows.Shapes.Path OpenPath = new System.Windows.Shapes.Path
+                {
+                    Data = OpenPathGeometry,
+                    Fill = PrimaryBackgroundColor,
+                    StrokeThickness = 1
+                };
+                System.Windows.Shapes.Path BlockPath = new System.Windows.Shapes.Path
+                {
+                    Data = BlockPathGeometry,
+                    Fill = PrimaryBackgroundColor,
+                    StrokeThickness = 1
+                };
+                System.Windows.Shapes.Path SavePath = new System.Windows.Shapes.Path
+                {
+                    Data = SavePathGeometry,
+                    Fill = PrimaryBackgroundColor,
+                    StrokeThickness = 1
+                };
+                System.Windows.Shapes.Path DeletePath = new System.Windows.Shapes.Path
+                {
+                    Data = DeletePathGeometry,
+                    Fill = PrimaryBackgroundColor,
+                    StrokeThickness = 1
+                };
+
                 Button SaveChangesBtn = new Button
                 {
-                    Content = "Д",
+                    Content = SavePath,
+                    Width = 50,
+                    Style = RoundedButtonStyle,
                     Margin = new Thickness(5)
                 };
 
                 Button EditBtn = new Button {
-                    Content = "Р",
+                    Content = BlockPath,
+                    Width = 50,
+                    Style = RoundedButtonStyle,
                     Margin = new Thickness(5)
                 };
                 
                 Button DeleteBtn = new Button {
-                    Content = "У",
+                    Content = DeletePath,
+                    Width = 50,
+                    Style = RoundedButtonStyle,
                     Margin = new Thickness(5)
                 };
 
                 SaveChangesBtn.Click += (s, e) =>
                 {
+                    Models.Teacher entityToEdit = _dbContext.Teachers.Find(ItemDockPanel.Tag);
+                    if (_dbContext != null && entityToEdit != null)
+                    {
+                        entityToEdit.FullName = FullNameTextBox.Text;
+                        entityToEdit.TelephoneNumber = Convert.ToInt32(TelephoneNumberTextBox.Text);
+                        entityToEdit.WorkingHours = WorkingHoursTextBox.Text;
                     
+                        _dbContext.SaveChanges();
+
+                        MessageBox.Show($"Данные {entityToEdit.FullName} успешно сохранены", "Сохранение изменений", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
                 };
 
                 EditBtn.Click += (s, e) =>
                 {
-                    if (textBox1b.IsReadOnly)
+                    if (FullNameTextBox.IsReadOnly)
                     {
-                        textBox1b.IsReadOnly = false;
-                        textBox2b.IsReadOnly = false;
-                        textBox3b.IsReadOnly = false;
+                        EditBtn.Content = OpenPath;
+                        FullNameTextBox.IsReadOnly = false;
+                        TelephoneNumberTextBox.IsReadOnly = false;
+                        WorkingHoursTextBox.IsReadOnly = false;
                     }
                     else
                     {
-                        textBox1b.IsReadOnly = true;
-                        textBox2b.IsReadOnly = true;
-                        textBox3b.IsReadOnly = true;
+                        EditBtn.Content = BlockPath;
+                        FullNameTextBox.IsReadOnly = true;
+                        TelephoneNumberTextBox.IsReadOnly = true;
+                        WorkingHoursTextBox.IsReadOnly = true;
+                    }
+                };
+
+                DeleteBtn.Click += (s, e) =>
+                {
+                    Models.Teacher entityToDelete = _dbContext.Teachers.Find(ItemDockPanel.Tag);
+                    if (_dbContext != null && entityToDelete != null)
+                    {
+                        _dbContext.Teachers.Remove(entityToDelete);
+                        _dbContext.SaveChanges();
                     }
                 };
                 
