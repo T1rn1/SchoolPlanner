46b365f03daf10c37a90fd31053cdd2049967508
arseny <arsenyvolodko@gmail.com>
2024-10-31 16:22:03 +0300

    Выносим создание нового элемента в отделюную функцию

--- SchoolPlanner/Pages/Teacher.xaml.cs
+++ SchoolPlanner/Pages/Teacher.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using SchoolPlanner.Models;
+using System.DirectoryServices;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
@@ -37,234 +38,242 @@ namespace SchoolPlanner.Pages
             SavePathGeometry = (PathGeometry)FindResource("save");
             DeletePathGeometry = (PathGeometry)FindResource("delete");
 
-            CreateStackPanelWithData();
+            FillStackPanel();
         }
 
-        public void CreateStackPanelWithData()
+        public void CreateStackPanel(Models.Teacher item)
         {
-            StackPanel MainStackPanel = new StackPanel
+            DockPanel ItemDockPanel = new DockPanel
+            {
+                HorizontalAlignment = HorizontalAlignment.Stretch,
+                VerticalAlignment = VerticalAlignment.Center,
+                Tag = item.Id,
+            };
+
+            Border border = new Border
+            {
+                CornerRadius = new CornerRadius(10),
+                Background = PrimaryBackgroundColor,
+                Child = ItemDockPanel,
+                Padding = new Thickness(3)
+            };
+            StackPanel LeftStackPanel = new StackPanel
+            {
+                Orientation = Orientation.Horizontal,
+                HorizontalAlignment = HorizontalAlignment.Left,
+                VerticalAlignment = VerticalAlignment.Center,
+            };
+            StackPanel innerStackPanel1 = new StackPanel
             {
                 Orientation = Orientation.Vertical,
-                HorizontalAlignment = HorizontalAlignment.Stretch,
-                VerticalAlignment = VerticalAlignment.Top,
+                Margin = new Thickness(10)
             };
-            MainGrid.Children.Add(MainStackPanel);
+            TextBlock FullNameTextBlock = new TextBlock
+            {
+                Text = "ФИО",
+                Foreground = TextTertiaryColor,
+                FontSize = 15,
+                FontWeight = FontWeights.Bold,
+                Margin = new Thickness(10),
+            };
+            TextBox FullNameTextBox = new TextBox
+            {
+                Text = item.FullName,
+                Margin = new Thickness(10),
+                Style = placeholderTextBoxStyle,
+                IsReadOnly = true,
+                MaxWidth = 250,
+            };
 
+            innerStackPanel1.Children.Add(FullNameTextBlock);
+            innerStackPanel1.Children.Add(FullNameTextBox);
+
+            StackPanel innerStackPanel2 = new StackPanel
+            {
+                Orientation = Orientation.Vertical,
+                Margin = new Thickness(10)
+            };
+
+            TextBlock TelephoneNumberTextBlock = new TextBlock
+            {
+                Text = "Телефон",
+                Foreground = TextTertiaryColor,
+                FontWeight = FontWeights.Bold,
+                FontSize = 15,
+                Margin = new Thickness(10),
+            };
+
+            TextBox TelephoneNumberTextBox = new TextBox
+            {
+                Text = item.TelephoneNumber.ToString(),
+                Margin = new Thickness(10),
+                Style = placeholderTextBoxStyle,
+                IsReadOnly = true,
+                Width = 100,
+            };
+
+            innerStackPanel2.Children.Add(TelephoneNumberTextBlock);
+            innerStackPanel2.Children.Add(TelephoneNumberTextBox);
+
+            StackPanel innerStackPanel3 = new StackPanel
+            {
+                Orientation = Orientation.Vertical,
+                Margin = new Thickness(10)
+            };
+
+            TextBlock WorkingHoursTextBlock = new TextBlock
+            {
+                Text = "Рабочее время",
+                Foreground = TextTertiaryColor,
+                FontWeight = FontWeights.Bold,
+                FontSize = 15,
+                Margin = new Thickness(10),
+            };
+
+            TextBox WorkingHoursTextBox = new TextBox
+            {
+                Text = item.WorkingHours,
+                Margin = new Thickness(10),
+                Style = placeholderTextBoxStyle,
+                IsReadOnly = true,
+                Width = 100,
+            };
+
+            innerStackPanel3.Children.Add(WorkingHoursTextBlock);
+            innerStackPanel3.Children.Add(WorkingHoursTextBox);
+
+            LeftStackPanel.Children.Add(innerStackPanel1);
+            LeftStackPanel.Children.Add(innerStackPanel2);
+            LeftStackPanel.Children.Add(innerStackPanel3);
+
+            ItemDockPanel.Children.Add(LeftStackPanel);
+
+            StackPanel RightStackPanel = new StackPanel
+            {
+                Orientation = Orientation.Horizontal,
+                HorizontalAlignment = HorizontalAlignment.Right,
+                VerticalAlignment = VerticalAlignment.Center,
+
+                Margin = new Thickness(10)
+            };
+
+            System.Windows.Shapes.Path OpenPath = new System.Windows.Shapes.Path
+            {
+                Data = OpenPathGeometry,
+                Fill = PrimaryBackgroundColor,
+                StrokeThickness = 1
+            };
+            System.Windows.Shapes.Path BlockPath = new System.Windows.Shapes.Path
+            {
+                Data = BlockPathGeometry,
+                Fill = PrimaryBackgroundColor,
+                StrokeThickness = 1
+            };
+            System.Windows.Shapes.Path SavePath = new System.Windows.Shapes.Path
+            {
+                Data = SavePathGeometry,
+                Fill = PrimaryBackgroundColor,
+                StrokeThickness = 1
+            };
+            System.Windows.Shapes.Path DeletePath = new System.Windows.Shapes.Path
+            {
+                Data = DeletePathGeometry,
+                Fill = PrimaryBackgroundColor,
+                StrokeThickness = 1
+            };
+
+            Button SaveChangesBtn = new Button
+            {
+                Content = SavePath,
+                Width = 50,
+                Style = RoundedButtonStyle,
+                Margin = new Thickness(5)
+            };
+
+            Button EditBtn = new Button
+            {
+                Content = BlockPath,
+                Width = 50,
+                Style = RoundedButtonStyle,
+                Margin = new Thickness(5)
+            };
+
+            Button DeleteBtn = new Button
+            {
+                Content = DeletePath,
+                Width = 50,
+                Style = RoundedButtonStyle,
+                Margin = new Thickness(5)
+            };
+
+            SaveChangesBtn.Click += (s, e) =>
+            {
+                Models.Teacher entityToEdit = _dbContext.Teachers.Find(ItemDockPanel.Tag);
+                if (_dbContext != null && entityToEdit != null)
+                {
+                    entityToEdit.FullName = FullNameTextBox.Text;
+                    entityToEdit.TelephoneNumber = Convert.ToInt32(TelephoneNumberTextBox.Text);
+                    entityToEdit.WorkingHours = WorkingHoursTextBox.Text;
+
+                    _dbContext.SaveChanges();
+
+                    MessageBox.Show($"Данные {entityToEdit.FullName} успешно сохранены", "Сохранение изменений", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                    FillStackPanel();
+                }
+            };
+
+            EditBtn.Click += (s, e) =>
+            {
+                if (FullNameTextBox.IsReadOnly)
+                {
+                    EditBtn.Content = OpenPath;
+                    FullNameTextBox.IsReadOnly = false;
+                    TelephoneNumberTextBox.IsReadOnly = false;
+                    WorkingHoursTextBox.IsReadOnly = false;
+                }
+                else
+                {
+                    EditBtn.Content = BlockPath;
+                    FullNameTextBox.IsReadOnly = true;
+                    TelephoneNumberTextBox.IsReadOnly = true;
+                    WorkingHoursTextBox.IsReadOnly = true;
+                }
+            };
+
+            DeleteBtn.Click += (s, e) =>
+            {
+                Models.Teacher entityToDelete = _dbContext.Teachers.Find(ItemDockPanel.Tag);
+                if (_dbContext != null && entityToDelete != null)
+                {
+                    _dbContext.Teachers.Remove(entityToDelete);
+                    _dbContext.SaveChanges();
+                }
+                MessageBox.Show($"Данные {entityToDelete.FullName} успешно удален", "Удаление", MessageBoxButton.OK, MessageBoxImage.Information);
+                FillStackPanel();
+            };
+
+            RightStackPanel.Children.Add(SaveChangesBtn);
+            RightStackPanel.Children.Add(EditBtn);
+            RightStackPanel.Children.Add(DeleteBtn);
+            ItemDockPanel.Children.Add(RightStackPanel);
+            MainStackPanel.Children.Add(border);
+        }
+
+        public void FillStackPanel()
+        {
+            MainStackPanel.Children.Clear();
             List<Models.Teacher> teachers = _dbContext.Teachers.ToList();
 
             foreach (var item in teachers)
             {
-                DockPanel ItemDockPanel = new DockPanel
-                {
-                    HorizontalAlignment = HorizontalAlignment.Stretch,
-                    VerticalAlignment = VerticalAlignment.Center,
-                    Tag = item.Id,
-                };
-
-                Border border = new Border {
-                    CornerRadius = new CornerRadius(10),
-                    Background = PrimaryBackgroundColor,
-                    Child = ItemDockPanel,
-                    Margin = new Thickness(10),
-                    Padding = new Thickness(3)
-                };
-
-                StackPanel LeftStackPanel = new StackPanel
-                {
-                    Orientation = Orientation.Horizontal,
-                    HorizontalAlignment = HorizontalAlignment.Left,
-                    VerticalAlignment = VerticalAlignment.Center,
-                };
-
-                StackPanel innerStackPanel1 = new StackPanel
-                {
-                    Orientation = Orientation.Vertical,
-                    Margin = new Thickness(10)
-                };
-
-                TextBlock FullNameTextBlock = new TextBlock
-                {
-                    Text = "ФИО",
-                    Foreground = TextTertiaryColor,
-                    FontWeight = FontWeights.Bold,
-                    Margin = new Thickness(10),
-                };
-
-                TextBox FullNameTextBox = new TextBox
-                {
-                    Text = item.FullName,
-                    Margin = new Thickness(10),
-                    Style = placeholderTextBoxStyle,
-                    IsReadOnly = true,
-                    MaxWidth = 250,
-                };
-
-                innerStackPanel1.Children.Add(FullNameTextBlock);
-                innerStackPanel1.Children.Add(FullNameTextBox);
-
-                StackPanel innerStackPanel2 = new StackPanel
-                {
-                    Orientation = Orientation.Vertical,
-                    Margin = new Thickness(10)
-                };
-
-                TextBlock TelephoneNumberTextBlock = new TextBlock
-                {
-                    Text = "Телефон",
-                    Foreground = TextTertiaryColor,
-                    FontWeight = FontWeights.Bold,
-                    Margin = new Thickness(10),
-                };
-
-                TextBox TelephoneNumberTextBox = new TextBox
-                {
-                    Text = item.TelephoneNumber.ToString(),
-                    Margin = new Thickness(10),
-                    Style = placeholderTextBoxStyle,
-                    IsReadOnly = true,
-                    Width = 100,
-                };
-
-                innerStackPanel2.Children.Add(TelephoneNumberTextBlock);
-                innerStackPanel2.Children.Add(TelephoneNumberTextBox);
-
-                StackPanel innerStackPanel3 = new StackPanel
-                {
-                    Orientation = Orientation.Vertical,
-                    Margin = new Thickness(10)
-                };
-
-                TextBlock WorkingHoursTextBlock = new TextBlock
-                {
-                    Text = "Рабочее время",
-                    Foreground = TextTertiaryColor,
-                    FontWeight = FontWeights.Bold,
-                    Margin = new Thickness(10),
-                };
-
-                TextBox WorkingHoursTextBox = new TextBox
-                {
-                    Text = item.WorkingHours,
-                    Margin = new Thickness(10),
-                    Style = placeholderTextBoxStyle,
-                    IsReadOnly = true,
-                    Width = 100,
-                };
-
-                innerStackPanel3.Children.Add(WorkingHoursTextBlock);
-                innerStackPanel3.Children.Add(WorkingHoursTextBox);
-
-                LeftStackPanel.Children.Add(innerStackPanel1);
-                LeftStackPanel.Children.Add(innerStackPanel2);
-                LeftStackPanel.Children.Add(innerStackPanel3);
-
-                ItemDockPanel.Children.Add(LeftStackPanel);
-
-                StackPanel RightStackPanel = new StackPanel
-                {
-                    Orientation = Orientation.Horizontal,
-                    HorizontalAlignment = HorizontalAlignment.Right,
-                    VerticalAlignment = VerticalAlignment.Center,
-                    
-                    Margin = new Thickness(10)
-                };
-
-                System.Windows.Shapes.Path OpenPath = new System.Windows.Shapes.Path
-                {
-                    Data = OpenPathGeometry,
-                    Fill = PrimaryBackgroundColor,
-                    StrokeThickness = 1
-                };
-                System.Windows.Shapes.Path BlockPath = new System.Windows.Shapes.Path
-                {
-                    Data = BlockPathGeometry,
-                    Fill = PrimaryBackgroundColor,
-                    StrokeThickness = 1
-                };
-                System.Windows.Shapes.Path SavePath = new System.Windows.Shapes.Path
-                {
-                    Data = SavePathGeometry,
-                    Fill = PrimaryBackgroundColor,
-                    StrokeThickness = 1
-                };
-                System.Windows.Shapes.Path DeletePath = new System.Windows.Shapes.Path
-                {
-                    Data = DeletePathGeometry,
-                    Fill = PrimaryBackgroundColor,
-                    StrokeThickness = 1
-                };
-
-                Button SaveChangesBtn = new Button
-                {
-                    Content = SavePath,
-                    Width = 50,
-                    Style = RoundedButtonStyle,
-                    Margin = new Thickness(5)
-                };
-
-                Button EditBtn = new Button {
-                    Content = BlockPath,
-                    Width = 50,
-                    Style = RoundedButtonStyle,
-                    Margin = new Thickness(5)
-                };
-                
-                Button DeleteBtn = new Button {
-                    Content = DeletePath,
-                    Width = 50,
-                    Style = RoundedButtonStyle,
-                    Margin = new Thickness(5)
-                };
-
-                SaveChangesBtn.Click += (s, e) =>
-                {
-                    Models.Teacher entityToEdit = _dbContext.Teachers.Find(ItemDockPanel.Tag);
-                    if (_dbContext != null && entityToEdit != null)
-                    {
-                        entityToEdit.FullName = FullNameTextBox.Text;
-                        entityToEdit.TelephoneNumber = Convert.ToInt32(TelephoneNumberTextBox.Text);
-                        entityToEdit.WorkingHours = WorkingHoursTextBox.Text;
-
-                        _dbContext.SaveChanges();
-
-                        MessageBox.Show($"Данные {entityToEdit.FullName} успешно сохранены", "Сохранение изменений", MessageBoxButton.OK, MessageBoxImage.Information);
-                    }
-                };
-
-                EditBtn.Click += (s, e) =>
-                {
-                    if (FullNameTextBox.IsReadOnly)
-                    {
-                        EditBtn.Content = OpenPath;
-                        FullNameTextBox.IsReadOnly = false;
-                        TelephoneNumberTextBox.IsReadOnly = false;
-                        WorkingHoursTextBox.IsReadOnly = false;
-                    }
-                    else
-                    {
-                        EditBtn.Content = BlockPath;
-                        FullNameTextBox.IsReadOnly = true;
-                        TelephoneNumberTextBox.IsReadOnly = true;
-                        WorkingHoursTextBox.IsReadOnly = true;
-                    }
-                };
-
-                DeleteBtn.Click += (s, e) =>
-                {
-                    Models.Teacher entityToDelete = _dbContext.Teachers.Find(ItemDockPanel.Tag);
-                    if (_dbContext != null && entityToDelete != null)
-                    {
-                        _dbContext.Teachers.Remove(entityToDelete);
-                        _dbContext.SaveChanges();
-                    }
-                };
-                
-                RightStackPanel.Children.Add(SaveChangesBtn);
-                RightStackPanel.Children.Add(EditBtn);
-                RightStackPanel.Children.Add(DeleteBtn);
-                ItemDockPanel.Children.Add(RightStackPanel);
-                MainStackPanel.Children.Add(border);
+                CreateStackPanel(item);
             }
+        }
+
+        private void AddNewTeacherBtn_Click(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
